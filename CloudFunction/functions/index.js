ac75e021526c6fb05309abef38cf80589214dd55
Sitthiphong Muntrima <sitthiphong.mun@gmail.com>
2020-10-06 14:03:18 +0700

    - Update cloud functions

--- CloudFunction/functions/index.js
+++ CloudFunction/functions/index.js
@@ -11,16 +11,19 @@ const functions = require('firebase-functions');
 // });
 
 exports.updateParkingCount = functions.database.ref('/status').onWrite((change, context) => {
-
+    //functions.logger.info('Cound Functions: On realtime data write');
     // Only edit data when it is first created.
-    if (change.before.exists()) {
-        return null;
-    }
-    // Exit when the data is deleted.
-    if (!change.after.exists()) {
-        return null;
-    }
-
+    // if (change.before.exists()) {
+    //     functions.logger.info('Cound Functions: Only edit data when it is first created, Return!!');
+    //     return null;
+    // }
+    // // Exit when the data is deleted.
+    // if (!change.after.exists()) {
+    //     functions.logger.info('Cound Functions: Exit when the data is deleted, Return!!');
+    //     return null;
+    // }
+    //functions.logger.info('Cound Functions: Ref: ' + change.after.ref);
+    //functions.logger.info('Cound Functions: Parrent: ' + change.after.parent);
     var blank = 0;
     var busy = 0;
     change.after.forEach((childSnapshot) => {
@@ -36,7 +39,8 @@ exports.updateParkingCount = functions.database.ref('/status').onWrite((change, 
         }
     });
 
-    console.log("Blank: " + blank + ", Bussy: " + busy);
+    //functions.logger.info('Cound Functions: Blank: ' + blank + ', Bussy: ' + busy);
     change.after.ref.parent.child('/count/blank').set(blank);
-    return change.after.ref.parent.child('/count/busy').set(busy);
+    change.after.ref.parent.child('/count/busy').set(busy);
+    return change.after.ref.parent.child('/count/all').set(blank + ',' + busy);
 });
